b8253c9600c977041e7e6548f86472dfdbada3f9
Tom Kornis <tomk@appdome.com>
2023-02-11 18:45:11 +0200

    added some card methods

--- discard-solitaire/src/app/app.component.ts
+++ discard-solitaire/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Card } from './models';
 import { CardsHelper } from './cards-helper';
 
@@ -7,10 +7,38 @@ import { CardsHelper } from './cards-helper';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'discard-solitaire';
   helper: CardsHelper = new CardsHelper();
   public deck: Card[] = this.helper.getDeck();
   public firstCardOfDeck = { ...this.deck[0], isShown: true};
   public test =  this.firstCardOfDeck.img
+  cardStacks: Card[][] = [[], [], [], []];
+
+  public firstCard: any
+
+  ngOnInit(): void {
+    this.deck = this.helper.getDeck();
+    this.dealFirstToStacks();
+    this.dealToStacks();
+    this.discard(this.cardStacks[0]);
+  }
+
+  private dealFirstToStacks(): void {
+    this.cardStacks = this.cardStacks.map(() => {
+        return [{ ...this.deck.pop(), isShown: true }];
+      }
+    )
+  };
+
+  private dealToStacks(): void {
+    this.cardStacks = this.cardStacks.map(stack => {
+        return [this.deck.pop(), ...stack];
+      }
+    )
+  }
+
+  public discard(stack: Card[]): void {
+    stack.pop();
+  }
 }
